a1b9c14bc12bbce946fdb44e384abf7132fc37c5
Hriday2406 <78265700+Hriday2406@users.noreply.github.com>
2024-12-25 23:53:54 +0530

    Add form validation

--- controller/newController.js
+++ controller/newController.js
@@ -1,19 +1,36 @@
 const db = require("../models/db");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
-const newPostHandler = asyncHandler(async (req, res) => {
-  let { username, messageText } = req.body;
+const newPostHandler = [
+  [
+    body("username")
+      .trim()
+      .isLength({ max: 20 })
+      .withMessage("Username should be less than 20 characters."),
+    body("messageText")
+      .trim()
+      .isLength({ max: 200 })
+      .withMessage("Message should not be more than 200 words."),
+  ],
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).render("newPage", { errors: errors.array() });
 
-  username = username.trim();
-  messageText = messageText.trim();
-  if (!username || username.length === 0)
-    throw new Error("Username is not valid");
-  if (!messageText || messageText.length === 0)
-    throw new Error("Message is not valid");
+    let { username, messageText } = req.body;
 
-  let status = await db.addNewMessage(username, messageText);
-  if (status) res.redirect("/");
-  else throw new Error("Error in uploading to Data Base");
-});
+    username = username.trim();
+    messageText = messageText.trim();
+    if (!username || username.length === 0)
+      throw new Error("Username is not valid");
+    if (!messageText || messageText.length === 0)
+      throw new Error("Message is not valid");
+
+    let status = await db.addNewMessage(username, messageText);
+    if (status) res.redirect("/");
+    else throw new Error("Error in uploading to Data Base");
+  }),
+];
 
 module.exports = { newPostHandler };
